c44bdf255fd3b4ac69625b7f05a8dcb3be1761e1
Ahya <ayakurdi@gmail.com>
2017-05-02 16:46:00 -0700

    added a get route

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
+add.get('/', function (req, res){
+  var message= "I don't know what I'm doing"
+  res.send(message)
+})
+
 add.listener(3000, function(){
   console.log("Server is working on port:",PORT)
 })
